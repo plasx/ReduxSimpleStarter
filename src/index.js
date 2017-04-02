1d87d13417a4e1c204b99a099b935434adf1e5ab
Daniel Rivera <plasx@Daniels-MacBook-Pro.local>
2017-04-01 20:43:44 -0400

    built out applicatoin, created some classes and functions having props being passed into them, also updated state depending on the constructor passing props to everything in the main app 'this.props' and in the functiosn 'props.whatever'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'SOME KEY HERE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+		this.state = { videos: [] };
+
+		YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			// this.setState({ videos: videos });
+		});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);		
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
